bf4a1bca5019bc830c74622d65dffaff02fe467d
mellogat <megatsalmanshukri@gmail.com>
2023-01-30 15:43:22 +0100

    Initial Commit

---
+++ LeiMap/LeiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeiMapApp.swift
+//  LeiMap
+//
+//  Created by megat megat salman shukri on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
