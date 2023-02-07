e003704143d222ec32f6d2cddeb4316c953cb40b
aronaNg <arona.ngom07@gmail.com>
2023-02-07 14:37:29 +0100

    add profile view

---
+++ LeiMap/UserProfilView.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfilView.swift
+//  LeiMap
+//
+//  Created by ngom arona on 07/02/2023.
+//
+
+import SwiftUI
+
+struct UserProfilView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserProfilView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfilView()
+    }
+}
