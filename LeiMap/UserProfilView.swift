c6c68e9ef585f3ad8a57e4f5db794932e92f92e3
aronaNg <arona.ngom07@gmail.com>
2023-02-07 14:49:18 +0100

    add background image

--- LeiMap/UserProfilView.swift
+++ LeiMap/UserProfilView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct UserProfilView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("photo")
+                .resizable()
+                .frame(height: 200)
+                . aspectRatio(contentMode: .fit)
+                .  edgesIgnoringSafeArea(.all)
+            
+            Spacer()
+        }
     }
 }
 
