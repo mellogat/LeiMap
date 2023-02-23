a10d3ccdc8e22229ce61bf86a73166a1144b8273
aronaNg <arona.ngom07@gmail.com>
2023-02-23 16:40:38 +0100

    map view

--- LeiMap/View/MapView.swift
+++ LeiMap/View/MapView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 import MapKit
 
+
+struct Location: Identifiable {
+    let id = UUID()
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+}
+
+extension Location {
+    static func getLocations() -> [Location] {
+        return [
+            Location(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 51.589, longitude: 4.77)),
+            Location(title: "Dakar", coordinate: CLLocationCoordinate2D(latitude: 51.55, longitude: 5.05 ))
+        ]
+    }
+}
+
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 47.6407,
-                                           longitude: 6.84168),
-            latitudinalMeters: 750,
-            longitudinalMeters: 750
-        )
+    
+    @State private var locations: [Location] = Location.getLocations()
+    @State private var trackingMode = MapUserTrackingMode.follow
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: 4.9), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    
     var body: some View {
-        Map(coordinateRegion: $region)
+        NavigationView {
+            Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true, userTrackingMode: $trackingMode, annotationItems: locations) { (location) -> MapAnnotation<PinView> in
+                MapAnnotation(coordinate: location.coordinate) { PinView(title: location.title)}
+            }.navigationBarHidden(true)
+        }
     }
 }
 
---
+++ LeiMap/View/PinView.swift
@@ -0,0 +1,53 @@
+//
+//  PinView.swift
+//  LeiMap
+//
+//  Created by ngom arona on 23/02/2023.
+//
+
+import SwiftUI
+
+struct PinView: View {
+    var title: String
+    @State var clicked = false
+    @State private var action: Int? = 0
+    
+    var body: some View {
+        if !clicked {
+            return AnyView(
+                Button(action: {
+                    clicked = true
+                }) {
+                    Image(systemName: "paperplane")
+                        .font(.system(size: 20.0))
+                        .foregroundColor(.red)
+                }
+            )
+        } else {
+            return AnyView (
+                VStack {
+                    NavigationLink(destination: Text(title), tag: 1, selection: $action) {
+                        EmptyView()
+                    }
+                    Text(title)
+                        .frame(width: 140, height: 40)
+                        .padding()
+                        .font(.system(size: 20.0))
+                        .background(Color.white)
+                        .border(Color.red, width: 2)
+                        .onTapGesture {
+                            self.action = 1
+                        }
+                }
+            )
+        }
+    }
+}
+
+//
+//struct PinView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PinView()
+//    }
+//}
+
